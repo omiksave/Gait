1ba70aceada8cebe41c29388e7e162e159f263fe
Omik Save <27330809+omiksave@users.noreply.github.com>
2020-10-06 14:58:11 -0700

    Update Plot.m

--- Plot.m
+++ Plot.m
@@ -4,11 +4,14 @@ b_up = b(1,1); b_low = b(1,2);
 bc = 0; % Center of damping
 sn = 0.95; % Sensitivity
 e = 0.13; % Lever arm
-[b,a] = butter(2,7/1000);
-%% Initializing damping controller
+[num,den] = butter(2,7/1000);
+
+
+%%
 tx_max = 1; tx_min = 1; k = 1; kp = 1;  kn = 1;
 %% Kalman filter parameter intialize
 T = 1/2000; %Sample Time
+Tx = 0.5/1000; %Sample Time
 A = [1 T 0.5*T^2;0 1 T;0 0 1];
 G = [0.1667*T^3;0.5*T^2;T];
 C = [1 0 0];
@@ -42,18 +45,24 @@ twenty = twenty1-base;% Substracting offset from +20
 ank_mul = ((((fifteen*4)+(twenty*3))/(2*60))^-1)*0.01745;% Calculating multiplier (rad/V)
 %% File read
 clearvars Input1
-data = readdat('data1');
+data = readdat('data');
 ankle = 57.296*data.data(:,1);% Converting ankle angle to degrees from radians
 %ankle = readdat('raw2'); ankle = ankle.data(:,1);
 %% Estimate noise in GRF
 T = 1/2000;
 force = normalforce(data.data(:,8),T);
 %% Get Heel Strikes
-[frame,strike,ankle_norm,avg_time1] = gaitphase(force,data.data(:,1));
+[frame,strike,ankle_norm,avg_time] = gaitphase(force,data.data(:,1));
 %% Save force and frame
 save('4','force','frame')
 %% Plotting robot dynamics
 robodyn(data,force,frame,strike,'Walking 0 Nms/rad')
 %% Plotting ankle dyanmics
-[m2y,d2y,t2y] = norgait(ankle_norm,frame(1:round(4/7*length(frame))-1));
+[m2,d2,t2] = norgait(ankle_norm,frame);
 [mt,dt] = norgait(-0.25*data.data(:,6),frame);
+%% Plotting nominal ankle (Stiffness only)
+nom_x1 = linspace(0,100,length(m1(1:t1)));
+nom_y1 = m1(1:t1);
+%% Plotting reference stiffness curve, Lee/Rousse et. al
+stiff_x = linspace(0,100,length(m(1:t)));
+stiff_y = stiff_ref(linspace(0,54.667,length(m(1:t))));
