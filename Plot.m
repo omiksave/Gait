ddb8d5946d6e0e478c01546687753723c54fcb52
Omik Save <27330809+omiksave@users.noreply.github.com>
2020-10-06 15:01:32 -0700

    Update Plot.m

--- Plot.m
+++ Plot.m
@@ -4,9 +4,7 @@ b_up = b(1,1); b_low = b(1,2);
 bc = 0; % Center of damping
 sn = 0.95; % Sensitivity
 e = 0.13; % Lever arm
-[num,den] = butter(2,7/1000);
-
-
+[b,a] = butter(2,20/1000);
 %%
 tx_max = 1; tx_min = 1; k = 1; kp = 1;  kn = 1;
 %% Kalman filter parameter intialize
@@ -47,7 +45,6 @@ ank_mul = ((((fifteen*4)+(twenty*3))/(2*60))^-1)*0.01745;% Calculating multiplie
 clearvars Input1
 data = readdat('data');
 ankle = 57.296*data.data(:,1);% Converting ankle angle to degrees from radians
-%ankle = readdat('raw2'); ankle = ankle.data(:,1);
 %% Estimate noise in GRF
 T = 1/2000;
 force = normalforce(data.data(:,8),T);
@@ -59,10 +56,10 @@ save('4','force','frame')
 robodyn(data,force,frame,strike,'Walking 0 Nms/rad')
 %% Plotting ankle dyanmics
 [m2,d2,t2] = norgait(ankle_norm,frame);
-[mt,dt] = norgait(-0.25*data.data(:,6),frame);
+%[mt,dt] = norgait(-0.25*data.data(:,6),frame);
 %% Plotting nominal ankle (Stiffness only)
-nom_x1 = linspace(0,100,length(m1(1:t1)));
-nom_y1 = m1(1:t1);
+nom_x = linspace(0,100,length(m(1:t)));
+nom_y = m(1:t);
 %% Plotting reference stiffness curve, Lee/Rousse et. al
 stiff_x = linspace(0,100,length(m(1:t)));
 stiff_y = stiff_ref(linspace(0,54.667,length(m(1:t))));
