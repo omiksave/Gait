d45b5ab127cb54f548fa067a3b4e668b9b36dc95
Omik Save <27330809+omiksave@users.noreply.github.com>
2020-10-08 22:01:40 -0700

    Update emg_analysis.m

--- emg_analysis.m
+++ emg_analysis.m
@@ -1,6 +1,6 @@
 clearvars Input1
 mvc = readdat('mvc');
-[b1,a1] = butter(2,5/1000);
+[b1,a1] = butter(2,4/1000);
 ta_max = max(filtfilt(b1,a1,abs(mvc.data(:,1)-mean(ta_base))));
 gas_max = max(filtfilt(b1,a1,abs(mvc.data(:,2)-mean(gas_base))));
 sol_max = max(filtfilt(b1,a1,abs(mvc.data(:,3)-mean(sol_base))));
@@ -8,7 +8,7 @@ pl_max = max(filtfilt(b1,a1,abs(mvc.data(:,4)-mean(pl_base))));
 %%
 clearvars emg
 emg = readdat('emg1');
-[b1,a1] = butter(2,5/1000);
+[b1,a1] = butter(2,4/1000);
 ta = filtfilt(b1,a1,abs(emg.data(:,1)-mean(ta_base)));
 gas = filtfilt(b1,a1,abs(emg.data(:,2)-mean(gas_base)));
 sol = filtfilt(b1,a1,abs(emg.data(:,3)-mean(sol_base)));
@@ -17,10 +17,9 @@ ta(1:96) = []; sol(1:96) = [];gas(1:96) = []; pl(1:96) = [];
 %%
 framex = frame(1:end-1);
 %%
-mean_ta4 = norgait(ta,framex);
-mean_sol4 = norgait(sol,framex);
-mean_gas4 = norgait(gas,framex);
-
+[met1,det1] = norgait((ta/ta_max)*100,framex);
+[mes1,des1] = norgait((sol/sol_max)*100,framex);
+[meg1,deg1] = norgait((gas/gas_max)*100,framex);
 %%
 figure;
 suptitle('Muscle Activity (No Robot)')
