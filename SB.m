b1deea09dc8e90aad925226c49d14f0dfb6c86f2
Omik Save <27330809+omiksave@users.noreply.github.com>
2021-01-03 15:24:10 -0700

    Create SB.m

---
+++ SB.m
@@ -0,0 +1,18 @@
+function SB(PWS)
+t = tic; % Initializing Clock
+PWS = PWS*1000;
+prev = 10000;
+while (toc(t)/60)<=28
+    if floor((toc(t)/60))~= prev
+        if (toc(t)/60)-2>0 && (toc(t)/60)-2<=10
+            setTreadmill(PWS*(1+(5*floor((toc(t)/60)-2)/100)),PWS,250,250);
+            ceil(toc(t)/60)
+            prev = floor((toc(t)/60));
+        else
+            setTreadmill(PWS,PWS,250,250);
+            ceil(toc(t)/60)
+            prev = floor((toc(t)/60));
+        end
+    end 
+end
+setTreadmill(0,0,250,250);
