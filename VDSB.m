f155dd1f4511d390472fee636ea069632f461710
Omik Save <27330809+omiksave@users.noreply.github.com>
2021-03-26 16:48:32 -0700

    Add files via upload

---
+++ VDSB.m
@@ -0,0 +1,13 @@
+function VDSB(MyClient,b1,b2,lim,PWS)
+PWS = PWS*1000;prev = 10000;
+t = tic;
+while (toc(t)/60)<=28
+    [LHEE,RHEE,LF,RF] = frameupdate(MyClient);
+    if LF<=40 && RHEE>LHEE && (toc(t)/60)-3<=11
+            bar_draw(max(0,RHEE-LHEE),[],b1,b2,lim);
+    elseif RF<=40 && LHEE>RHEE && (toc(t)/60)-3<=11
+            bar_draw([],(1+max(0,(3*floor((toc(t)/60)-2)/100)))*max(0,LHEE-RHEE),b1,b2,lim);
+    end
+    prev = SB_dep(PWS,prev,t);
+end
+setTreadmill(0,0,250,250);
