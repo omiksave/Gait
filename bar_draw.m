fb3272774553f3e0725aeeb32e87cb8282fae928
Omik Save <27330809+omiksave@users.noreply.github.com>
2021-03-26 16:37:36 -0700

    Update bar_draw.m

--- bar_draw.m
+++ bar_draw.m
@@ -1,4 +1,8 @@
-function bar_draw(LSL,RSL,b1)
-set(b1,'Ydata',[LSL,RSL]);
-drawnow
+function bar_draw(LSL,RSL,b1,b2,lim)
+if isempty(RSL)
+    set(b2,'Xdata',linspace(lim,lim-LSL,60));
+else 
+    set(b1,'Xdata',linspace(lim,lim-RSL,60));
+end
+drawnow;
 end
