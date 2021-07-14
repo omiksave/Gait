83ca14edbe43cbf8651221727f1b1093f080139f
Omik Save <27330809+omiksave@users.noreply.github.com>
2021-07-13 18:15:39 -0700

    Update SB_dep.m

--- SB_dep.m
+++ SB_dep.m
@@ -1,13 +1,13 @@
 function prev = SB_dep(PWS,prev,t)
 
 if floor((toc(t)/60))~= prev
-    if (toc(t)/60)-2>0 && (toc(t)/60)-2<=11
-        setTreadmill(PWS*(1+(5*floor((toc(t)/60)-2)/100)),PWS,250,250);
-        ceil(toc(t)/60)
+    if floor(toc(t)/60)-2<=10
+        setTreadmill(PWS*(1+max(0,(5*floor((toc(t)/60)-2)/100))),PWS,250,250);
+        floor(toc(t)/60)
         prev = floor((toc(t)/60));
     else
         setTreadmill(PWS,PWS,250,250);
-        ceil(toc(t)/60)
+        floor(toc(t)/60)
         prev = floor((toc(t)/60));
     end
 end 
