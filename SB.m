a9709c8eb85eeea58ff5b27f529ae48f96306ac9
Omik Save <27330809+omiksave@users.noreply.github.com>
2021-07-13 18:15:35 -0700

    Update SB.m

--- SB.m
+++ SB.m
@@ -4,13 +4,13 @@ PWS = PWS*1000;
 prev = 10000;
 while (toc(t)/60)<=28
     if floor((toc(t)/60))~= prev
-        if (toc(t)/60)-2>0 && (toc(t)/60)-2<=11
+        if floor(toc(t)/60)-2>0 && floor(toc(t)/60)-2<=10
             setTreadmill(PWS*(1+(5*floor((toc(t)/60)-2)/100)),PWS,250,250);
-            ceil(toc(t)/60)
+            floor(toc(t)/60)
             prev = floor((toc(t)/60));
         else
             setTreadmill(PWS,PWS,250,250);
-            ceil(toc(t)/60)
+            floor(toc(t)/60)
             prev = floor((toc(t)/60));
         end
     end 
