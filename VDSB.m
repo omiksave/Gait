3497d0d2cef214ec825234bbaec2e69a01b0e6ac
Omik Save <27330809+omiksave@users.noreply.github.com>
2021-07-13 18:16:11 -0700

    Update VDSB.m

--- VDSB.m
+++ VDSB.m
@@ -3,10 +3,10 @@ PWS = PWS*1000;prev = 10000;
 t = tic;
 while (toc(t)/60)<=28
     [LHEE,RHEE,LF,RF] = frameupdate(MyClient);
-    if LF<=40 && RHEE>LHEE && (toc(t)/60)-3<=11
+    if LF<=40 && RHEE>LHEE && floor(toc(t)/60)-2<=10
             bar_draw(max(0,RHEE-LHEE),[],b1,b2,lim);
-    elseif RF<=40 && LHEE>RHEE && (toc(t)/60)-3<=11
-            bar_draw([],(1+max(0,(3*floor((toc(t)/60)-2)/100)))*max(0,LHEE-RHEE),b1,b2,lim);
+    elseif RF<=40 && LHEE>RHEE && floor(toc(t)/60)-2<=10
+            bar_draw([],(1-max(0,(3*floor((toc(t)/60)-2)/100)))*max(0,LHEE-RHEE),b1,b2,lim);
     end
     prev = SB_dep(PWS,prev,t);
 end
