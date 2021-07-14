ee75e201a9cc2d2211317957480f4329d66132d2
Omik Save <27330809+omiksave@users.noreply.github.com>
2021-07-13 18:16:09 -0700

    Update VD.m

--- VD.m
+++ VD.m
@@ -2,10 +2,10 @@ function VD(MyClient,b1,b2,lim)
 t = tic;
 while (toc(t)/60)<=28
     [LHEE,RHEE,LF,RF] = frameupdate(MyClient);
-    if LF<=40 && RHEE>LHEE && (toc(t)/60)-3<=11
+    if LF<=40 && RHEE>LHEE && floor(toc(t)/60)-2<=10
             bar_draw(max(0,RHEE-LHEE),[],b1,b2,lim);
-    elseif RF<=40 && LHEE>RHEE && (toc(t)/60)-3<=11
-            bar_draw([],(1+max(0,(3*floor((toc(t)/60)-2)/100)))*max(0,LHEE-RHEE),b1,b2,lim);
+    elseif RF<=40 && LHEE>RHEE && floor(toc(t)/60)-2<=10
+            bar_draw([],(1-max(0,(3*floor((toc(t)/60)-2)/100)))*max(0,LHEE-RHEE),b1,b2,lim);
     end
-    ceil(toc(t)/60)
+    floor(toc(t)/60)
 end
